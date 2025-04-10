f2e292a97bd27b4ed0bf4c35630caf1134e57c87
Ayim William <odhisayim@gmail.com>
2025-04-10 12:02:15 +0300

    added button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import Welcome from "./components/Welcome";
 import './App.css'
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Button from "./components/UI-elements/Button";
 
 const App = () => {
   return (
@@ -11,6 +12,18 @@ const App = () => {
       <Navbar />
       <main className="main-content">
         <Welcome name="Hildah" title="software engineer"/>
+        <div className="button-container">
+          <Button 
+            text="Click Me"
+            color="blue"
+            onClick={()=>alert('Button Clicked!')}
+          />
+          <Button 
+            text="Click Me"
+            color="green"
+            onClick={()=>alert('Button Clicked!')}
+          />
+        </div>
 
       </main>
       <Footer />
