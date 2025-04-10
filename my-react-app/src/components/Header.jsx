77c7284d3fea0f5609e99122befb40545bb28471
Ayim William <odhisayim@gmail.com>
2025-04-10 10:15:15 +0300

    added Header jsx component

---
+++ my-react-app/src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header(){
+
+    
+    return(
+        <header className="header">
+            <h1 className="header-title">
+                PRODUCT PALACE
+            </h1>
+        </header>
+    )
+}
+
+export default Header;
