d12bbb85e86ea3a25d1f077e8cfab6a5f8891908
Ayim William <odhisayim@gmail.com>
2025-04-10 11:32:47 +0300

    added Navbar & navbar css

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import './App.css'
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Navbar />
       <main className="main-content">
         <Welcome name="Hildah" title="software engineer"/>
 
---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css'
+
+function Navbar(){
+    return (
+        <nav className="navbar">
+            <ul className="nav-list"> 
+                <li className="nav-list-item">Home</li>
+                <li className="nav-list-item">About</li>
+                <li className="nav-list-item">Contact</li>
+            </ul>
+        </nav>
+        
+    )
+}
+
+export default Navbar;
